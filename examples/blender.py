5a6f47e5745bc48d1ac164803ddc5877b25ae5ad
shadeofblue <blue@wave460.net>
2020-07-29 22:55:44 +0200

    (rm syspath changes)

--- examples/blender.py
+++ examples/blender.py
@@ -1,8 +1,3 @@
-import os
-import sys
-
-sys.path.append(os.path.realpath('..'))
-
 from yapapi.runner import Engine, Task, vm
 from yapapi.runner.ctx import WorkContext
 from datetime import timedelta
