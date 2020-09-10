87abcf55f2a8edd987415a987ef2afcdd3262efc
shadeofblue <blue@wave460.net>
2020-09-10 19:26:14 +0200

    fix version check :/ ...

--- yapapi/__init__.py
+++ yapapi/__init__.py
@@ -2,9 +2,7 @@
 import logging
 
 from pathlib import Path
-from single_version import get_version  # type: ignore
-
-__version__: str = get_version("yapapi", Path(__file__).parent.parent)
+from pkg_resources import get_distribution
 
 
 def enable_default_logger(
@@ -19,3 +17,19 @@ def enable_default_logger(
     logger.addHandler(handler)
     logger.setLevel(level)
     logger.disabled = False
+
+
+def get_version() -> str:
+    pyproject_path = Path(__file__).parents[1] / "pyproject.toml"
+    if pyproject_path.exists():
+        import toml
+
+        with open(pyproject_path) as f:
+            pyproject = toml.loads(f.read())
+
+        return pyproject["tool"]["poetry"]["version"]
+
+    return get_distribution("yapapi").version
+
+
+__version__: str = get_version()
