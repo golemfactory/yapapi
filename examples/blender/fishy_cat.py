0d5610071f6da4ad7753a98d4c4cea25c94381a3
Piotr Chromiec <tworec@golem.network>
2020-12-02 10:46:30 +0100

    fishy_cat example
    source: https://download.blender.org/demo/test/splash_fishy_cat_2.zip

---
+++ examples/blender/fishy_cat.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+import asyncio
+from datetime import timedelta
+import pathlib
+import sys
+
+from yapapi import (
+    Executor,
+    Task,
+    __version__ as yapapi_version,
+    WorkContext,
+    windows_event_loop_fix,
+)
+from yapapi.log import enable_default_logger, log_summary, log_event_repr  # noqa
+from yapapi.package import vm
+from yapapi.rest.activity import BatchTimeoutError
+
+# For importing `utils.py`:
+script_dir = pathlib.Path(__file__).resolve().parent
+parent_directory = script_dir.parent
+sys.stderr.write(f"Adding {parent_directory} to sys.path.\n")
+sys.path.append(str(parent_directory))
+import utils  # noqa
+
+
+async def main(subnet_tag: str):
+    package = await vm.repo(
+        image_hash="9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae",
+        min_mem_gib=0.5,
+        min_storage_gib=2.0,
+    )
+
+    async def worker(ctx: WorkContext, tasks):
+        scene_path = str(script_dir / "fishy_cat.blend")
+        ctx.send_file(scene_path, "/golem/resource/scene.blend")
+        async for task in tasks:
+            frame = task.data
+            crops = [{"outfilebasename": "out", "borders_x": [0.0, 1.0], "borders_y": [0.0, 1.0]}]
+            ctx.send_json(
+                "/golem/work/params.json",
+                {
+                    "scene_file": "/golem/resource/scene.blend",
+                    "resolution": (300, 150),
+                    "use_compositing": False,
+                    "crops": crops,
+                    "samples": 100,
+                    "frames": [frame],
+                    "output_format": "PNG",
+                    "RESOURCES_DIR": "/golem/resources",
+                    "WORK_DIR": "/golem/work",
+                    "OUTPUT_DIR": "/golem/output",
+                },
+            )
+            ctx.run("/golem/entrypoints/run-blender.sh")
+            output_file = f"output_fc_{frame}.png"
+            ctx.download_file(f"/golem/output/out{frame:04d}.png", output_file)
+            try:
+                # Set timeout for executing the script on the provider. Two minutes is plenty
+                # of time for computing a single frame, for other tasks it may be not enough.
+                # If the timeout is exceeded, this worker instance will be shut down and all
+                # remaining tasks, including the current one, will be computed by other providers.
+                yield ctx.commit(timeout=timedelta(seconds=1200))
+                # TODO: Check if job results are valid
+                # and reject by: task.reject_task(reason = 'invalid file')
+                task.accept_result(result=output_file)
+            except BatchTimeoutError:
+                print(
+                    f"{utils.TEXT_COLOR_RED}"
+                    f"Task timed out: {task}, time: {task.running_time}"
+                    f"{utils.TEXT_COLOR_DEFAULT}"
+                )
+                raise
+
+    # iterator over the frame indices that we want to render
+    frames: range = range(0, 10, 10)
+    # TODO make this dynamic, e.g. depending on the size of files to transfer
+    # worst-case time overhead for initialization, e.g. negotiation, file transfer etc.
+    init_overhead: timedelta = timedelta(minutes=20)
+
+    # By passing `event_consumer=log_summary()` we enable summary logging.
+    # See the documentation of the `yapapi.log` module on how to set
+    # the level of detail and format of the logged information.
+    async with Executor(
+        package=package,
+        max_workers=1,
+        budget=5.0,
+        timeout=init_overhead + timedelta(minutes=len(frames) * 2),
+        subnet_tag=subnet_tag,
+        event_consumer=log_summary(log_event_repr),
+    ) as executor:
+
+        async for task in executor.submit(worker, [Task(data=frame) for frame in frames]):
+            print(
+                f"{utils.TEXT_COLOR_CYAN}"
+                f"Task computed: {task}, result: {task.result}, time: {task.running_time}"
+                f"{utils.TEXT_COLOR_DEFAULT}"
+            )
+
+
+if __name__ == "__main__":
+    parser = utils.build_parser("Render blender scene")
+    parser.set_defaults(log_file="blender-yapapi.log")
+    args = parser.parse_args()
+
+    # This is only required when running on Windows with Python prior to 3.8:
+    windows_event_loop_fix()
+
+    enable_default_logger(log_file=args.log_file)
+
+    loop = asyncio.get_event_loop()
+
+    subnet = args.subnet_tag
+    sys.stderr.write(
+        f"yapapi version: {utils.TEXT_COLOR_YELLOW}{yapapi_version}{utils.TEXT_COLOR_DEFAULT}\n"
+    )
+    sys.stderr.write(f"Using subnet: {utils.TEXT_COLOR_YELLOW}{subnet}{utils.TEXT_COLOR_DEFAULT}\n")
+    task = loop.create_task(main(subnet_tag=args.subnet_tag))
+    try:
+        loop.run_until_complete(task)
+    except KeyboardInterrupt:
+        print(
+            f"{utils.TEXT_COLOR_YELLOW}"
+            "Shutting down gracefully, please wait a short while "
+            "or press Ctrl+C to exit immediately..."
+            f"{utils.TEXT_COLOR_DEFAULT}"
+        )
+        task.cancel()
+        try:
+            loop.run_until_complete(task)
+            print(
+                f"{utils.TEXT_COLOR_YELLOW}"
+                "Shutdown completed, thank you for waiting!"
+                f"{utils.TEXT_COLOR_DEFAULT}"
+            )
+        except (asyncio.CancelledError, KeyboardInterrupt):
+            pass
