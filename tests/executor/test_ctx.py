982d7dfed08014b66a1c3c50f0fa130ff6b8b9f3
shadeofblue <blue@wave460.net>
2021-03-29 18:58:12 +0200

    add `WorkContext.send_bytes` command

--- tests/executor/test_ctx.py
+++ tests/executor/test_ctx.py
@@ -1,5 +1,12 @@
-from yapapi.executor.ctx import CommandContainer
+import factory
 import json
+import pytest
+import sys
+from unittest import mock
+
+from yapapi.executor.ctx import CommandContainer, WorkContext
+
+from tests.factories.props import NodeInfoFactory
 
 
 def test_command_container():
@@ -29,3 +36,51 @@ def test_command_container():
     ]
     """
     assert json.loads(expected_commands) == c.commands()
+
+
+class TestWorkContext:
+    @staticmethod
+    def _get_work_context(storage):
+        return WorkContext(
+            factory.Faker("pystr"),
+            node_info=NodeInfoFactory(),
+            storage=storage
+        )
+
+    @staticmethod
+    def _assert_dst_path(steps, dst_path):
+        c = CommandContainer()
+        steps.register(c)
+        assert c.commands().pop()['transfer']['to'] == f"container:{dst_path}"
+
+    @pytest.mark.asyncio
+    @pytest.mark.skipif(
+        sys.version_info < (3, 8), reason="AsyncMock requires python 3.8+"
+    )
+    async def test_send_json(self):
+        storage = mock.AsyncMock()
+        dst_path = "/test/path"
+        data = {
+            'param': 'value',
+        }
+        ctx = self._get_work_context(storage)
+        ctx.send_json(dst_path, data)
+        steps = ctx.commit()
+        await steps.prepare()
+        storage.upload_bytes.assert_called_with(json.dumps(data).encode("utf-8"))
+        self._assert_dst_path(steps, dst_path)
+
+    @pytest.mark.asyncio
+    @pytest.mark.skipif(
+        sys.version_info < (3, 8), reason="AsyncMock requires python 3.8+"
+    )
+    async def test_send_bytes(self):
+        storage = mock.AsyncMock()
+        dst_path = "/test/path"
+        data = b"some byte string"
+        ctx = self._get_work_context(storage)
+        ctx.send_bytes(dst_path, data)
+        steps = ctx.commit()
+        await steps.prepare()
+        storage.upload_bytes.assert_called_with(data)
+        self._assert_dst_path(steps, dst_path)
