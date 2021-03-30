cab9f94f25946d62a242fc83a22aa7c8ac4999bc
shadeofblue <blue@wave460.net>
2021-03-30 13:36:53 +0200

    use aiohttp's general `_request_timeout`

--- yapapi/rest/activity.py
+++ yapapi/rest/activity.py
@@ -169,7 +169,7 @@ class PollingBatch(Batch):
                 results: List[yaa.ExeScriptCommandResult] = await self._api.get_exec_batch_results(
                     self._activity_id,
                     self._batch_id,
-                    # timeout=timeout  # timeout argument is currently unsupported
+                    _request_timeout = timeout
                 )
             except ApiException as err:
                 if err.status == 408:
