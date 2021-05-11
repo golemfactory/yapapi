94df0ee6799210afdf4107858dce0bb89fdbace1
shadeofblue <blue@wave460.net>
2021-05-11 13:41:50 +0200

    make `Payload` an AutodecoratingModel

--- examples/turbogeth/turbogeth.py
+++ examples/turbogeth/turbogeth.py
@@ -2,7 +2,7 @@ import asyncio
 
 from dataclasses import dataclass
 
-from yapapi.props.builder import DemandBuilder, AutodecoratingModel
+from yapapi.props.builder import DemandBuilder
 from yapapi.props.base import prop, constraint
 from yapapi.props import inf
 
@@ -14,7 +14,7 @@ PROP_TURBOGETH_RPC_PORT = "golem.srv.app.eth.rpc-port"
 
 
 @dataclass
-class Turbogeth(Payload, AutodecoratingModel):
+class TurbogethPayload(Payload):
     rpc_port: int = prop(PROP_TURBOGETH_RPC_PORT, default=None)
 
     runtime: str = constraint(inf.INF_RUNTIME_NAME, default=TURBOGETH_RUNTIME_NAME)
@@ -24,7 +24,7 @@ class Turbogeth(Payload, AutodecoratingModel):
 
 async def main():
     builder = DemandBuilder()
-    await builder.decorate(Turbogeth(rpc_port=1234))
+    await builder.decorate(TurbogethPayload(rpc_port=1234))
     print(builder)
 
 
--- yapapi/payload/__init__.py
+++ yapapi/payload/__init__.py
@@ -1,7 +1,7 @@
 import abc
 
-from yapapi.props.builder import DemandDecorator
+from yapapi.props.builder import AutodecoratingModel
 
 
-class Payload(DemandDecorator, abc.ABC):
+class Payload(AutodecoratingModel, abc.ABC):
     """Base class for descriptions of the payload required by the requestor."""
