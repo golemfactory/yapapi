abf22114176bbb5c883595ccac3f0c2e993a07db
shadeofblue <blue@wave460.net>
2021-05-19 17:59:48 +0200

    update the toy example with the HL Services API

--- examples/simple-service-poc/simple_service.py
+++ examples/simple-service-poc/simple_service.py
@@ -4,7 +4,6 @@ the requestor agent controlling and interacting with the "simple service"
 """
 import asyncio
 from datetime import datetime, timedelta
-import itertools
 import json
 import pathlib
 import random
@@ -13,16 +12,16 @@ import sys
 
 
 from yapapi import (
-    Executor,
     NoPaymentAccountError,
-    Task,
     __version__ as yapapi_version,
     WorkContext,
     windows_event_loop_fix,
 )
+from yapapi.executor import Golem
+from yapapi.executor.services import Service
+
 from yapapi.log import enable_default_logger, log_summary, log_event_repr  # noqa
 from yapapi.package import vm
-from yapapi.rest.activity import BatchTimeoutError
 
 examples_dir = pathlib.Path(__file__).resolve().parent.parent
 sys.path.append(str(examples_dir))
@@ -36,113 +35,104 @@ from utils import (
 )
 
 
-async def main(subnet_tag, driver=None, network=None):
-    package = await vm.repo(
+class SimpleService(Service):
+    STATS_PATH = "/golem/out/stats"
+    PLOT_INFO_PATH = "/golem/out/plot"
+    SIMPLE_SERVICE = "/golem/run/simple_service.py"
+
+    plots_to_download = None
+
+    def post_init(self):
+        self.plots_to_download = []
+
+    @staticmethod
+    async def get_payload():
+        return await vm.repo(
         image_hash="8b11df59f84358d47fc6776d0bb7290b0054c15ded2d6f54cf634488",
         min_mem_gib=0.5,
         min_storage_gib=2.0,
     )
 
-    async def service(ctx: WorkContext, tasks):
-        STATS_PATH = "/golem/out/stats"
-        PLOT_INFO_PATH = "/golem/out/plot"
-        SIMPLE_SERVICE = "/golem/run/simple_service.py"
+    async def on_plot(self, out: bytes):
+        fname = json.loads(out.strip())
+        print(f"{TEXT_COLOR_CYAN}plot: {fname}{TEXT_COLOR_DEFAULT}")
+        self.plots_to_download.append(fname)
 
-        ctx.run("/golem/run/simulate_observations_ctl.py", "--start")
-        ctx.send_bytes(
-            "/golem/in/get_stats.sh", f"{SIMPLE_SERVICE} --stats > {STATS_PATH}".encode()
+    @staticmethod
+    async def on_stats(out: bytes):
+        print(f"{TEXT_COLOR_CYAN}stats: {out}{TEXT_COLOR_DEFAULT}")
+
+    async def start(self):
+        self.ctx.run("/golem/run/simulate_observations_ctl.py", "--start")
+        self.ctx.send_bytes(
+            "/golem/in/get_stats.sh", f"{self.SIMPLE_SERVICE} --stats > {self.STATS_PATH}".encode()
         )
-        ctx.send_bytes(
-            "/golem/in/get_plot.sh", f"{SIMPLE_SERVICE} --plot dist > {PLOT_INFO_PATH}".encode()
+        self.ctx.send_bytes(
+            "/golem/in/get_plot.sh", f"{self.SIMPLE_SERVICE} --plot dist > {self.PLOT_INFO_PATH}".encode()
         )
+        yield self.ctx.commit()
 
-        yield ctx.commit()
+    async def run(self):
+        while True:
+            await asyncio.sleep(10)
 
-        plots_to_download = []
+            self.ctx.run("/bin/sh", "/golem/in/get_stats.sh")
+            self.ctx.download_bytes(self.STATS_PATH, self.on_stats)
+            self.ctx.run("/bin/sh", "/golem/in/get_plot.sh")
+            self.ctx.download_bytes(self.PLOT_INFO_PATH, self.on_plot)
 
-        # have a look at asyncio docs and figure out whether to leave the callback or replace it with something
-        # more asyncio-ic
+            for plot in self.plots_to_download:
+                test_filename = (
+                        "".join(random.choice(string.ascii_letters) for _ in
+                                range(10)) + ".png"
+                )
+                self.ctx.download_file(plot, str(pathlib.Path(__file__).resolve().parent / test_filename))
+            yield self.ctx.commit()
 
-        async def on_plot(out: bytes):
-            nonlocal plots_to_download
-            fname = json.loads(out.strip())
-            print(f"{TEXT_COLOR_CYAN}plot: {fname}{TEXT_COLOR_DEFAULT}")
-            plots_to_download.append(fname)
+    async def shutdown(self):
+        self.ctx.run("/golem/run/simulate_observations_ctl.py", "--stop")
+        yield self.ctx.commit()
 
-        async def on_stats(out: bytes):
-            print(f"{TEXT_COLOR_CYAN}stats: {out}{TEXT_COLOR_DEFAULT}")
 
-        try:
-            async for task in tasks:
-                await asyncio.sleep(10)
-
-                ctx.run("/bin/sh", "/golem/in/get_stats.sh")
-                ctx.download_bytes(STATS_PATH, on_stats)
-                ctx.run("/bin/sh", "/golem/in/get_plot.sh")
-                ctx.download_bytes(PLOT_INFO_PATH, on_plot)
-                yield ctx.commit()
-
-                for plot in plots_to_download:
-                    test_filename = (
-                        "".join(random.choice(string.ascii_letters) for _ in range(10)) + ".png"
-                    )
-                    ctx.download_file(plot, pathlib.Path(__file__).resolve().parent / test_filename)
-                yield ctx.commit()
-
-                task.accept_result()
-
-        except (KeyboardInterrupt, asyncio.CancelledError):
-            # with the current implementation it won't work correctly
-            # because at this stage, the Executor is shutting down anyway
-            ctx.run("/golem/run/simulate_observations_ctl.py", "--stop")
-            yield ctx.commit()
-            raise
-
-    # Worst-case overhead, in minutes, for initialization (negotiation, file transfer etc.)
-    # TODO: make this dynamic, e.g. depending on the size of files to transfer
-    init_overhead = 3
-    # Providers will not accept work if the timeout is outside of the [5 min, 30min] range.
-    # We increase the lower bound to 6 min to account for the time needed for our demand to
-    # reach the providers.
-    min_timeout, max_timeout = 6, 30
-
-    timeout = timedelta(minutes=29)
-
-    # By passing `event_consumer=log_summary()` we enable summary logging.
-    # See the documentation of the `yapapi.log` module on how to set
-    # the level of detail and format of the logged information.
-    async with Executor(
-        package=package,
-        max_workers=1,
+async def main(subnet_tag, driver=None, network=None):
+    async with Golem(
         budget=1.0,
-        timeout=timeout,
         subnet_tag=subnet_tag,
         driver=driver,
         network=network,
         event_consumer=log_summary(log_event_repr),
-    ) as executor:
+    ) as golem:
 
         print(
             f"yapapi version: {TEXT_COLOR_YELLOW}{yapapi_version}{TEXT_COLOR_DEFAULT}\n"
             f"Using subnet: {TEXT_COLOR_YELLOW}{subnet_tag}{TEXT_COLOR_DEFAULT}, "
-            f"payment driver: {TEXT_COLOR_YELLOW}{executor.driver}{TEXT_COLOR_DEFAULT}, "
-            f"and network: {TEXT_COLOR_YELLOW}{executor.network}{TEXT_COLOR_DEFAULT}\n"
+            f"payment driver: {TEXT_COLOR_YELLOW}{golem.driver}{TEXT_COLOR_DEFAULT}, "
+            f"and network: {TEXT_COLOR_YELLOW}{golem.network}{TEXT_COLOR_DEFAULT}\n"
         )
 
         start_time = datetime.now()
 
-        async for task in executor.submit(service, (Task(data=n) for n in itertools.count(1))):
-            print(
-                f"{TEXT_COLOR_CYAN}"
-                f"Script executed: {task}, result: {task.result}, time: {task.running_time}"
-                f"{TEXT_COLOR_DEFAULT}"
-            )
+        cluster = await golem.run_service(SimpleService)
 
-        print(
-            f"{TEXT_COLOR_CYAN}"
-            f"Service finished, total time: {datetime.now() - start_time}"
-            f"{TEXT_COLOR_DEFAULT}"
-        )
+        def instances():
+            return [{s.ctx.id, s.state.value} for s in cluster.instances]
+
+        def still_running():
+            return any([s for s in cluster.instances if s.is_available])
+
+        while datetime.now() < start_time + timedelta(minutes=3):
+            print(f"instances: {instances()}")
+            await asyncio.sleep(3)
+
+        print("stopping instances")
+        cluster.stop()
+
+        cnt = 0
+        while cnt < 10 and still_running():
+            print(f"instances: {instances()}")
+            await asyncio.sleep(1)
+
+    print(f"instances: {instances()}")
 
 
 if __name__ == "__main__":
