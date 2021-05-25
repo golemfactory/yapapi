54b92ff9799a5431693930e47e4124c85da52555
Kuba Mazurek <zakaprov@gmail.com>
2021-05-25 10:32:02 +0200

    Refactor keyspace and attack worker functions

--- examples/yacat/yacat.py
+++ examples/yacat/yacat.py
@@ -2,14 +2,17 @@
 import argparse
 import asyncio
 from datetime import datetime, timedelta
-import pathlib
+from pathlib import Path
 import sys
+from tempfile import NamedTemporaryFile
+from typing import AsyncIterable
 
 from yapapi import Golem, NoPaymentAccountError, Task, WorkContext, windows_event_loop_fix
+from yapapi.executor.events import CommandExecuted
 from yapapi.log import enable_default_logger, log_summary, log_event_repr  # noqa
 from yapapi.payload import vm
 
-examples_dir = pathlib.Path(__file__).resolve().parent.parent
+examples_dir = Path(__file__).resolve().parent.parent
 sys.path.append(str(examples_dir))
 
 from utils import (
@@ -22,6 +25,7 @@ from utils import (
 )
 
 HASHCAT_ATTACK_MODE = 3  # stands for mask attack, hashcat -a option
+KEYSPACE_OUTPUT_PATH = Path("/golem/output/keyspace")
 
 arg_parser = build_parser("Run a hashcat attack (mask mode) on Golem network")
 arg_parser.add_argument("--hash", type=str, help="Target hash to be cracked")
@@ -43,19 +47,59 @@ arg_parser.add_argument(
 args = argparse.Namespace()
 
 
-def write_hash(hash):
-    with open("in.hash", "w") as f:
-        f.write(hash)
+async def compute_keyspace(context: WorkContext, tasks: AsyncIterable[Task]):
+    """Worker script which computes the size of the keyspace for the mask attack.
+
+    This function is used as the `worker` parameter to `Golem#execute_tasks`.
+    It represents a sequence of commands to be executed on a remote provider node.
+    """
+    async for task in tasks:
+        cmd = f"hashcat --keyspace " f"-a {HASHCAT_ATTACK_MODE} -m {args.hash_type} {args.mask}"
+        context.run("/bin/bash", "-c", cmd)
+
+        future_result = yield context.commit(timeout=timedelta(minutes=10))
+        # each item is the result of a single command on the provider (including setup commands)
+        result: List[CommandExecuted] = await future_result
+        # we take the last item since it's the last command that was executed on the provider
+        cmd_result: CommandExecuted = result[-1]
+
+        if cmd_result.success:
+            keyspace = int(cmd_result.stdout)
+        else:
+            raise RuntimeError("Failed to compute attack keyspace")
+
+        task.accept_result(result=keyspace)
 
 
-def write_keyspace_check_script(mask):
-    with open("keyspace.sh", "w") as f:
-        f.write(f"hashcat --keyspace -a 3 {mask} -m 400 > /golem/work/keyspace.txt")
+async def perform_attack(ctx: WorkContext, tasks: AsyncIterable[Task]):
+    """Worker script which performs a hashcat mask attack against a target hash.
+
+    This function is used as the `worker` parameter to `Golem#execute_tasks`.
+    It represents a sequence of commands to be executed on a remote provider node.
+    """
+    async for task in tasks:
+        skip = task.data
+        limit = skip + args.chunk_size
+        worker_output_path = f"/golem/output/hashcat_{skip}.potfile"
+
+        ctx.run(f"/bin/sh", "-c", _make_attack_command(skip, limit, worker_output_path))
+
+        output_file = NamedTemporaryFile()
+        ctx.download_file(worker_output_path, output_file.name)
+        # ExeOptions?
+        yield ctx.commit(timeout=timedelta(minutes=10))
+        task.accept_result(result=output_file.file.readline())
+        output_file.close()
 
 
-def read_keyspace():
-    with open("keyspace.txt", "r") as f:
-        return int(f.readline())
+def _make_attack_command(skip: int, limit: int, output_path: str) -> str:
+    return (
+        f"touch {output_path}; "
+        f"hashcat -a {HASHCAT_ATTACK_MODE} -m {args.hash_type} "
+        f"--self-test-disable --potfile-disable "
+        f"--skip={skip} --limit={limit} -o {output_path} "
+        f"'{args.hash}' '{args.mask}' || true"  # TODO this may cover up other errors
+    )
 
 
 async def main(args):
@@ -64,41 +108,6 @@ async def main(args):
         min_mem_gib=0.5,
         min_storage_gib=2.0,
     )
-
-    async def worker_check_keyspace(ctx: WorkContext, tasks):
-        async for task in tasks:
-            keyspace_sh_filename = "keyspace.sh"
-            ctx.send_file(keyspace_sh_filename, "/golem/work/keyspace.sh")
-            ctx.run("/bin/sh", "/golem/work/keyspace.sh")
-            output_file = "keyspace.txt"
-            ctx.download_file("/golem/work/keyspace.txt", output_file)
-            yield ctx.commit(timeout=timedelta(minutes=10))
-            task.accept_result()
-
-    async def worker_find_password(ctx: WorkContext, tasks):
-        ctx.send_file("in.hash", "/golem/work/in.hash")
-
-        async for task in tasks:
-            skip = task.data
-            limit = skip + args.chunk_size
-
-            # Commands to be run on the provider
-            commands = (
-                "rm -f /golem/work/*.potfile ~/.hashcat/hashcat.potfile; "
-                f"touch /golem/work/hashcat_{skip}.potfile; "
-                f"hashcat -a 3 -m 400 /golem/work/in.hash {args.mask} --skip={skip} --limit={limit} --self-test-disable -o /golem/work/hashcat_{skip}.potfile || true"
-            )
-            ctx.run(f"/bin/sh", "-c", commands)
-
-            output_file = f"hashcat_{skip}.potfile"
-            ctx.download_file(f"/golem/work/hashcat_{skip}.potfile", output_file)
-            yield ctx.commit(timeout=timedelta(minutes=10))
-            task.accept_result(result=output_file)
-
-    # beginning of the main flow
-
-    write_hash(args.hash)
-    write_keyspace_check_script(args.mask)
 
     # By passing `event_consumer=log_summary()` we enable summary logging.
     # See the documentation of the `yapapi.log` module on how to set
@@ -117,25 +126,18 @@ async def main(args):
             f"and network: {TEXT_COLOR_YELLOW}{golem.network}{TEXT_COLOR_DEFAULT}\n"
         )
 
-        keyspace_computed = False
         start_time = datetime.now()
 
         completed = golem.execute_tasks(
-            worker_check_keyspace,
-            [Task(data="calculate_keyspace")],
+            compute_keyspace,
+            [Task(data="compute_keyspace")],
             payload=package,
             max_workers=1,
             timeout=timedelta(minutes=30),
         )
 
         async for task in completed:
-            keyspace_computed = True
-
-        if not keyspace_computed:
-            # Assume the errors have been already reported and we may return quietly.
-            return
-
-        keyspace = read_keyspace()
+            keyspace = task.result
 
         print(
             f"{TEXT_COLOR_CYAN}"
@@ -147,7 +149,7 @@ async def main(args):
         max_workers = keyspace // args.chunk_size
 
         completed = golem.execute_tasks(
-            worker_find_password,
+            perform_attack,
             data,
             payload=package,
             max_workers=max_workers,
