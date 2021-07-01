376ed8e9c38989790ed959eba4762107b3212300
shadeofblue <blue@wave460.net>
2021-07-01 12:58:13 +0200

    black

--- tests/props/test_com.py
+++ tests/props/test_com.py
@@ -12,10 +12,7 @@ def test_com_linear_fixed_price():
 
 
 def test_com_linear_price_for():
-    com: ComLinear = ComLinearFactory(
-        linear_coeffs=LINEAR_COEFFS,
-        usage_vector=DEFINED_USAGES
-    )
+    com: ComLinear = ComLinearFactory(linear_coeffs=LINEAR_COEFFS, usage_vector=DEFINED_USAGES)
     assert com.price_for[Counter.CPU] == LINEAR_COEFFS[0]
     assert com.price_for[Counter.TIME] == LINEAR_COEFFS[1]
 
@@ -23,16 +20,24 @@ def test_com_linear_price_for():
 @pytest.mark.parametrize(
     "usage, cost",
     [
-        ([0.0, 0.0], LINEAR_COEFFS[-1],),
-        ([2.0, 0.0], LINEAR_COEFFS[0] * 2.0 + LINEAR_COEFFS[-1],),
-        ([0.0, 2.0], LINEAR_COEFFS[1] * 2.0 + LINEAR_COEFFS[-1],),
-        ([3.0, 5.0], LINEAR_COEFFS[0] * 3.0 + LINEAR_COEFFS[1] * 5.0 + LINEAR_COEFFS[-1],),
-    ]
+        (
+            [0.0, 0.0],
+            LINEAR_COEFFS[-1],
+        ),
+        (
+            [2.0, 0.0],
+            LINEAR_COEFFS[0] * 2.0 + LINEAR_COEFFS[-1],
+        ),
+        (
+            [0.0, 2.0],
+            LINEAR_COEFFS[1] * 2.0 + LINEAR_COEFFS[-1],
+        ),
+        (
+            [3.0, 5.0],
+            LINEAR_COEFFS[0] * 3.0 + LINEAR_COEFFS[1] * 5.0 + LINEAR_COEFFS[-1],
+        ),
+    ],
 )
 def test_com_linear_calculate_cost(usage, cost):
-    com: ComLinear = ComLinearFactory(
-        linear_coeffs=LINEAR_COEFFS,
-        usage_vector=DEFINED_USAGES
-    )
+    com: ComLinear = ComLinearFactory(linear_coeffs=LINEAR_COEFFS, usage_vector=DEFINED_USAGES)
     assert com.calculate_cost(usage) == cost
-
