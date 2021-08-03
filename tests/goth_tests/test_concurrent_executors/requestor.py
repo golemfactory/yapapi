c46e3623c0d36ba142633c3f0e7bd2285613365d
azawlocki <artur.zawlocki@golem.network>
2021-08-03 08:50:01 +0200

    Add integration test for concurrent executions of Golem.execute_task()

---
+++ tests/goth_tests/test_concurrent_executors/requestor.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+"""A requestor script for testing concurrent execution of Golem.execute_tasks().
+
+It creates a pipeline consisting of two `Golem.execute_tasks()` calls,
+with the results of the tasks computed by the first one (A) given as input data
+for the tasks computed by the second one (B).
+"""
+import asyncio
+import pathlib
+import sys
+
+from yapapi import Golem, Task
+from yapapi.log import enable_default_logger
+from yapapi.payload import vm
+
+
+async def main():
+
+    vm_package = await vm.repo(
+        image_hash="9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae",
+        min_mem_gib=0.5,
+        min_storage_gib=2.0,
+    )
+
+    first_task = True
+
+    async def duplicator(work_ctx, tasks):
+        """A worker that executes `echo {task.data} {task.data}` command on provider.
+
+        The command's output is the result of the whole task.
+
+        The first ever task computed by an instance of this worker fails.
+        It succeeds when re-tried. This may be used to test re-trying tasks,
+        creating new agreements and displaying summary information on task/activity
+        failures.
+        """
+        async for task in tasks:
+
+            nonlocal first_task
+
+            if first_task:
+                first_task = False
+                work_ctx.run("/bin/sleep", "1")
+                work_ctx.run("/command/not/found")
+
+            work_ctx.run("/bin/sleep", "1")
+            work_ctx.run("/bin/echo", task.data, task.data)
+            future_result = yield work_ctx.commit()
+            result = await future_result
+            output = result[-1].stdout.strip()
+            task.accept_result(output)
+
+    async with Golem(budget=1.0, subnet_tag="goth") as golem:
+
+        # Construct a pipeline:
+        #
+        #   input_tasks
+        #        |
+        #        V
+        #    [ Job ALEF ]
+        #        |
+        #        V
+        # intermediate_tasks
+        #        |
+        #        V
+        #    [ Job BET ]
+        #        |
+        #        V
+        #   output_tasks
+
+        input_tasks = [Task(s) for s in "01234567"]
+
+        computed_input_tasks = golem.execute_tasks(
+            duplicator,
+            input_tasks,
+            payload=vm_package,
+            max_workers=1,
+            job_id="ALEF",
+        )
+
+        async def intermediate_tasks():
+            async for task in computed_input_tasks:
+                print(f"ALEF computed task: {task.data} -> {task.result}", file=sys.stderr)
+                yield Task(data=task.result)
+
+        output_tasks = golem.execute_tasks(
+            duplicator,
+            intermediate_tasks(),
+            payload=vm_package,
+            max_workers=1,
+            job_id="BET",
+        )
+
+        async for task in output_tasks:
+            print(f"BET computed task: {task.data} -> {task.result}", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    test_dir = pathlib.Path(__file__).parent.name
+    enable_default_logger(log_file=f"{test_dir}.log")
+    asyncio.run(main())
