7c2aa844f9de503135f4cf4da95424cbaffb30a2
Filip <filip@golem.network>
2021-09-07 21:05:59 +0200

    Revert "Test"
    This reverts commit fce8c2ea0b1c4d7c6cb5e4a364ab9fe91f97153a.

--- tests/goth_tests/assertions.py
+++ tests/goth_tests/assertions.py
@@ -19,7 +19,6 @@ logger = logging.getLogger("goth.test.assertions")
 async def assert_no_errors(output_lines: EventStream[str]):
     """Assert that no output line contains the substring `ERROR`."""
     async for line in output_lines:
-        print(line)
         if "ERROR" in line:
             raise AssertionError("Command reported ERROR")
 
