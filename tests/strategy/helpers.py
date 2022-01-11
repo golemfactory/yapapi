345f6bd062221d31681449c2ac7751d0991960de
shadeofblue <blue@wave460.net>
2022-01-11 18:44:31 +0100

    use factory_boy factories for event mocks ...

--- tests/strategy/helpers.py
+++ tests/strategy/helpers.py
@@ -1,6 +1,3 @@
-from unittest.mock import Mock, PropertyMock, patch
-from contextlib import contextmanager
-
 from tests.factories.rest.market import OfferProposalFactory
 
 
@@ -10,14 +7,3 @@ def mock_offer(provider_id=1, coeffs=(0.001, 0.002, 0.1)):
     kwargs["proposal__proposal__issuer_id"] = provider_id
     kwargs["proposal__proposal__properties__linear_coeffs"] = list(coeffs)
     return OfferProposalFactory(**kwargs)
-
-
-@contextmanager
-def mock_event(cls, provider_id):
-    """Yield a `cls` event emitted for provider_id"""
-    event = cls(Mock(), Mock())
-    with patch(
-        "yapapi.events.AgreementEvent.provider_id", new_callable=PropertyMock
-    ) as mock_provider_id:
-        mock_provider_id.return_value = provider_id
-        yield event
--- tests/strategy/test_decrease_score_for_unconfirmed.py
+++ tests/strategy/test_decrease_score_for_unconfirmed.py
@@ -2,11 +2,14 @@ import asyncio
 import pytest
 
 from yapapi.strategy import MarketStrategy, DecreaseScoreForUnconfirmedAgreement
-from yapapi.events import AgreementRejected, AgreementConfirmed
 from yapapi import Golem
 
+from tests.factories.events import (
+    AgreementRejectedFactory as AgreementRejected,
+    AgreementConfirmedFactory as AgreementConfirmed,
+)
 
-from .helpers import mock_offer, mock_event
+from .helpers import mock_offer
 
 #   (events, providers_with_decreased_scores)
 sample_data = (
@@ -41,8 +44,8 @@ async def test_decrease_score_for(events_def, decreased_providers):
     strategy = DecreaseScoreForUnconfirmedAgreement(Always6(), 0.5)
 
     for event_cls, event_provider_id in events_def:
-        with mock_event(event_cls, event_provider_id) as event:
-            strategy.on_event(event)
+        event = event_cls(agreement__provider_id=event_provider_id)
+        strategy.on_event(event)
 
     for provider_id in (1, 2):
         offer = mock_offer(provider_id)
@@ -57,7 +60,6 @@ def empty_event_consumer(event):
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("events_def, decreased_providers", sample_data)
-@pytest.mark.skip(reason="This dosn't work because of some strange magic related to AsyncWrapper")
 async def test_full_DSFUA_workflow(dummy_yagna_engine, events_def, decreased_providers):
     """Test if DecreaseScoreForUnconfirmedAgreement is correctly initialized as a default strategy
 
@@ -68,8 +70,8 @@ async def test_full_DSFUA_workflow(dummy_yagna_engine, events_def, decreased_pro
     golem = Golem(budget=1, event_consumer=empty_event_consumer, app_key="NOT_A_REAL_APPKEY")
     async with golem:
         for event_cls, event_provider_id in events_def:
-            with mock_event(event_cls, event_provider_id) as event:
-                golem._engine._emit_event(event)
+            event = event_cls(agreement__provider_id=event_provider_id)
+            golem._engine._emit_event(event)
 
         await asyncio.sleep(0.1)  # let the events propagate
 
