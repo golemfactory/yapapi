add75fd2360712e13640d8833f0a1369838f8bb8
shadeofblue <blue@wave460.net>
2022-01-13 10:02:27 +0100

    create rather than `new` ...

--- tests/factories/rest/market.py
+++ tests/factories/rest/market.py
@@ -41,11 +41,11 @@ class OfferProposalFactory(factory.Factory):
         model = OfferProposal
 
     @classmethod
-    def new(cls, provider_id=1, coeffs=(0.001, 0.002, 0.1)):
-        kwargs = {}
-        kwargs["proposal__proposal__issuer_id"] = provider_id
+    def create(cls, provider_id=None, coeffs=(0.001, 0.002, 0.1), **kwargs):
+        if provider_id:
+            kwargs["proposal__proposal__issuer_id"] = provider_id
         kwargs["proposal__proposal__properties__linear_coeffs"] = list(coeffs)
-        return cls(**kwargs)
+        return super().create(**kwargs)
 
     subscription = factory.SubFactory(SubscriptionFactory)
     proposal = factory.SubFactory(ProposalEventFactory)
