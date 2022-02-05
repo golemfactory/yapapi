57d9a15499a9afedcb8d83190ecd9f001d36bfde
shadeofblue <blue@wave460.net>
2022-02-05 10:43:24 +0100

    fix worker's exception handling

--- yapapi/executor/__init__.py
+++ yapapi/executor/__init__.py
@@ -231,7 +231,7 @@ class Executor:
                     work_context.emit(events.WorkerFinished)
                 except Exception as e:
                     work_context.emit(events.WorkerFinished, exc_info=sys.exc_info())  # type: ignore
-                    await task_gen.athrow(type(e))
+                    await task_gen.athrow(type(e), e)
                     raise
                 finally:
                     await self._engine.accept_payments_for_agreement(job.id, agreement.id)
