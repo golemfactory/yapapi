c9863bf0238dafb8d02afb0f95de4e2e579a00b8
Filip <filip@golem.network>
2022-02-16 14:54:13 +0100

    Fix

--- yapapi/utils.py
+++ yapapi/utils.py
@@ -4,7 +4,6 @@ from datetime import datetime, timezone, tzinfo
 import enum
 import functools
 import logging
-from os import environ
 from typing import AsyncContextManager, Callable, Optional
 import warnings
 
