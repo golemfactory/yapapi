b46f3a89af22bcaa1a8f82b6335c67a437280c75
Jan Betley <jan.betley@golem.network>
2022-02-22 20:20:19 +0100

    test for the new add_event_consumer interface

--- tests/test_add_event_consumer.py
+++ tests/test_add_event_consumer.py
@@ -61,3 +61,42 @@ async def test_emit_event(dummy_yagna_engine):
     assert got_events_2 == emitted_events
     assert got_events_3 == emitted_events
     assert got_events_4 == sample_events
+
+
+@pytest.mark.asyncio
+async def test_emit_event_class(dummy_yagna_engine):
+    got_events_1 = []
+    got_events_2 = []
+    got_events_3 = []
+
+    def event_consumer_1(event: events.Event) -> None:
+        got_events_1.append(event)
+
+    def event_consumer_2(event: events.Event) -> None:
+        got_events_2.append(event)
+
+    def event_consumer_3(event: events.Event) -> None:
+        got_events_3.append(event)
+
+    golem = Golem(budget=1, app_key="NOT_A_REAL_APPKEY")
+
+    await golem.add_event_consumer(event_consumer_1, events.SubscriptionEvent)
+    await golem.add_event_consumer(event_consumer_2, events.ServiceEvent)
+    await golem.add_event_consumer(event_consumer_3)
+    async with golem:
+        service_started = golem._engine.emit(
+            events.ServiceStarted, job="foo", agreement="bar", activity="baz", service="???"
+        )
+        subscription_created = golem._engine.emit(
+            events.SubscriptionCreated, job="foo", subscription="bar"
+        )
+        service_finished = golem._engine.emit(
+            events.ServiceFinished, job="foo", agreement="bar", activity="baz", service="???"
+        )
+
+    assert got_events_1 == [subscription_created]
+    assert got_events_2 == [service_started, service_finished]
+
+    #   Additional ShutdownFinished event is passed to the catchall consumer only
+    assert got_events_3[:3] == [service_started, subscription_created, service_finished]
+    assert len(got_events_3) == 4 and isinstance(got_events_3[3], events.ShutdownFinished)
