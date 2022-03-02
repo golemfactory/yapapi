fab28ee3406957059968c504c76018d02cfbc451
Jan Betley <jan.betley@golem.network>
2022-03-02 15:43:49 +0100

    Event prefiltering logic implemented

--- yapapi/event_dispatcher.py
+++ yapapi/event_dispatcher.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import Callable, List, Set, Type
+from typing import Callable, Dict, Set, Type
 
 from yapapi import events
 from yapapi.utils import AsyncWrapper
@@ -7,7 +7,7 @@ from yapapi.utils import AsyncWrapper
 
 class AsyncEventDispatcher:
     def __init__(self):
-        self._consumers: List[AsyncWrapper] = []
+        self._consumers: Dict[AsyncWrapper, Set[Type[events.Event]]] = {}
 
     def add_event_consumer(
         self,
@@ -18,11 +18,12 @@ class AsyncEventDispatcher:
         consumer = AsyncWrapper(event_consumer)
         if start_consumer:
             consumer.start()
-        self._consumers.append(consumer)
+        self._consumers[consumer] = event_classes
 
     def emit(self, event: events.Event):
-        for consumer in self._consumers:
-            consumer.async_call(event)
+        for consumer, event_classes in self._consumers.items():
+            if any(isinstance(event, event_cls) for event_cls in event_classes):
+                consumer.async_call(event)
 
     def start(self):
         for consumer in self._consumers:
