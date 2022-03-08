219c79e14439117513642500da073dc33d8a6261
Filip <filip@golem.network>
2022-03-08 10:36:56 +0100

    Mid-agreement payments integration test

---
+++ tests/goth_tests/test_mid_agreement_payments/requestor.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import asyncio
+from typing import AsyncIterable
+from datetime import timedelta
+
+from yapapi import Golem, Task, WorkContext
+from yapapi.log import enable_default_logger, log_event_repr
+from yapapi.payload import vm
+
+
+async def worker(context: WorkContext, tasks: AsyncIterable[Task]):
+    async for task in tasks:
+        script = context.new_script()
+        future_result = script.run("/bin/sh", "-c", "sleep 1000")
+
+        yield script
+
+        task.accept_result(result=await future_result)
+
+
+async def main():
+    package = await vm.repo(
+        image_hash="d646d7b93083d817846c2ae5c62c72ca0507782385a2e29291a3d376",
+    )
+
+    tasks = [Task(data=None)]
+    timeout = timedelta(hours=24)
+
+    async with Golem(
+        budget=10.0,
+        subnet_tag="goth",
+        event_consumer=log_event_repr,
+    ) as golem:
+        async for completed in golem.execute_tasks(worker, tasks, payload=package, timeout=timeout):
+            print(f"Task finished: {completed}.")
+
+
+if __name__ == "__main__":
+    enable_default_logger(log_file="long_expiration.log")
+
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(main())
+    loop.run_until_complete(task)
---
+++ tests/goth_tests/test_mid_agreement_payments/test_mid_agreement_payments.py
@@ -0,0 +1,49 @@
+"""A goth test scenario for mid-agreement payments."""
+from functools import partial
+import logging
+import os
+from pathlib import Path
+import re
+from typing import List
+
+import pytest
+
+from goth.configuration import load_yaml, Override
+from goth.runner.log import configure_logging
+from goth.runner import Runner
+from goth.runner.probe import RequestorProbe
+
+
+logger = logging.getLogger("goth.test.mid_agreement_payments")
+
+
+@pytest.mark.asyncio
+async def test_long_expiration(
+    log_dir: Path,
+    goth_config_path: Path,
+    config_overrides: List[Override],
+) -> None:
+
+    # This is the default configuration with 2 wasm/VM providers
+    goth_config = load_yaml(goth_config_path, config_overrides)
+    test_script_path = str(Path(__file__).parent / "requestor.py")
+
+    configure_logging(log_dir)
+
+    runner = Runner(
+        base_log_dir=log_dir,
+        compose_config=goth_config.compose_config,
+    )
+
+    async with runner(goth_config.containers):
+
+        requestor = runner.get_probes(probe_type=RequestorProbe)[0]
+
+        async with requestor.run_command_on_host(test_script_path, env=os.environ) as (
+            _cmd_task,
+            cmd_monitor,
+            _process_monitor,
+        ):
+            # Wait for executor shutdown
+            await cmd_monitor.wait_for_pattern("ShutdownFinished", timeout=2000)
+            logger.info("Requestor script finished")
