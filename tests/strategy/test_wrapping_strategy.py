aa03a482e26dd03181b6ee6d100c40ec7b894779
shadeofblue <blue@wave460.net>
2022-03-09 08:35:35 +0100

    base tests for `MarketStrategy` and `WrappingMarketStrategy`

---
+++ tests/strategy/test_wrapping_strategy.py
@@ -0,0 +1,27 @@
+from unittest import mock
+from yapapi.strategy import WrappingMarketStrategy
+
+
+class MyBaseStrategy(mock.Mock):
+    some_other_value = 123
+
+
+class MyStrategy(WrappingMarketStrategy):
+    some_value = 456
+
+    def some_method(self):
+        return True
+
+
+def test_wrapping_strategy():
+    base_strategy = MyBaseStrategy()
+    strategy = MyStrategy(base_strategy)
+
+    assert strategy.some_value == MyStrategy.some_value
+    assert strategy.some_other_value == MyBaseStrategy.some_other_value
+
+    strategy.some_method()
+    assert not base_strategy.some_method.called
+
+    strategy.some_other_method()
+    assert base_strategy.some_other_method.called
