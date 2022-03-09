e89b3a8568794542121af4c513b4f85d22484109
shadeofblue <blue@wave460.net>
2022-03-09 20:51:43 +0100

    + tests for `_enforce_debit_note_intervals`

--- tests/engine/test_debit_note_intervals.py
+++ tests/engine/test_debit_note_intervals.py
@@ -1,9 +1,12 @@
+from datetime import datetime, timedelta
 import functools
 import pytest
 import re
+import sys
 from unittest import mock
 
 from yapapi.engine import _Engine
+from yapapi.rest.market import Agreement
 from yapapi.rest.payment import DebitNote
 from yapapi.strategy import PROP_DEBIT_NOTE_INTERVAL_SEC, PROP_PAYMENT_TIMEOUT_SEC
 
@@ -11,15 +14,15 @@ from tests.factories.rest.market import AgreementFactory
 from tests.factories.rest.payment import DebitNoteFactory
 
 
-def mock_engine(debit_note: DebitNote, num_debit_notes=0, num_payable_debit_notes=0) -> _Engine:
+def mock_engine(
+    agreement: Agreement, debit_note: DebitNote, num_debit_notes=0, num_payable_debit_notes=0
+) -> _Engine:
     with mock.patch("yapapi.engine.rest.Configuration"):
         engine = _Engine(budget=0.0, strategy=mock.Mock(), event_consumer=mock.Mock())
 
-    engine._num_debit_notes[debit_note.activity_id] = num_debit_notes + 1  # noqa
+    engine._all_agreements[agreement.id] = agreement  # noqa
+    engine._num_debit_notes[debit_note.activity_id] = num_debit_notes  # noqa
     engine._num_payable_debit_notes[debit_note.activity_id] = num_payable_debit_notes  # noqa
-
-    if debit_note.payment_due_date:
-        engine._num_payable_debit_notes[debit_note.activity_id] += 1  # noqa
 
     return engine
 
@@ -125,7 +128,7 @@ def _message_matches(regex):
         ),
     ],
 )
-def test_debit_notes(
+def test_verify_debit_note_intervals(
     agreement_kwargs,
     debit_note_kwargs,
     duration,
@@ -135,8 +138,116 @@ def test_debit_notes(
     vpt_condition,
 ):
     agreement = AgreementFactory(**agreement_kwargs)
-    debit_note = DebitNoteFactory(**debit_note_kwargs)
-    engine = mock_engine(debit_note, num_debit_notes, num_payable_debit_notes)
+    debit_note = DebitNoteFactory(_base__agreement_id=agreement.id, **debit_note_kwargs)
+    engine = mock_engine(agreement, debit_note, num_debit_notes, num_payable_debit_notes)
+
+    engine._num_debit_notes[debit_note.activity_id] += 1  # noqa
+    if debit_note.payment_due_date:
+        engine._num_payable_debit_notes[debit_note.activity_id] += 1  # noqa
 
     assert vdni_condition(engine._verify_debit_note_interval(agreement, debit_note, duration))
     assert vpt_condition(engine._verify_payment_timeout(agreement, debit_note, duration))
+
+
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="AsyncMock requires python 3.8+")
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "agreement_kwargs, debit_note_kwargs, created_at_cb, "
+    "num_debit_notes, num_payable_debit_notes, "
+    "debit_note_accepted, agreement_terminated",
+    [
+        ({}, {}, datetime.now, 0, 0, True, False),
+        ({"terminated": True}, {}, datetime.now, 0, 0, False, False),
+        ({}, {}, lambda: None, 0, 0, False, False),
+        ({}, {"_base__payment_due_date": True}, datetime.now, 0, 0, False, True),
+        (
+            {"details___ref__demand__properties": {PROP_DEBIT_NOTE_INTERVAL_SEC: 100}},
+            {},
+            datetime.now,
+            0,
+            0,
+            False,
+            True,
+        ),
+        (
+            {"details___ref__demand__properties": {PROP_DEBIT_NOTE_INTERVAL_SEC: 100}},
+            {},
+            lambda: datetime.now() - timedelta(seconds=100),
+            0,
+            0,
+            True,
+            False,
+        ),
+        (
+            {"details___ref__demand__properties": {PROP_DEBIT_NOTE_INTERVAL_SEC: 100}},
+            {},
+            lambda: datetime.now() - timedelta(seconds=300),
+            2,
+            0,
+            True,
+            False,
+        ),
+        (
+            {"details___ref__demand__properties": {PROP_DEBIT_NOTE_INTERVAL_SEC: 100}},
+            {},
+            lambda: datetime.now() - timedelta(seconds=300),
+            3,
+            0,
+            False,
+            True,
+        ),
+        (
+            {"details___ref__demand__properties": {PROP_PAYMENT_TIMEOUT_SEC: 100}},
+            {"_base__payment_due_date": True},
+            datetime.now,
+            0,
+            0,
+            False,
+            True,
+        ),
+        (
+            {"details___ref__demand__properties": {PROP_PAYMENT_TIMEOUT_SEC: 100}},
+            {"_base__payment_due_date": True},
+            lambda: datetime.now() - timedelta(seconds=100),
+            0,
+            0,
+            True,
+            False,
+        ),
+        (
+            {"details___ref__demand__properties": {PROP_PAYMENT_TIMEOUT_SEC: 100}},
+            {"_base__payment_due_date": True},
+            lambda: datetime.now() - timedelta(seconds=300),
+            0,
+            2,
+            True,
+            False,
+        ),
+        (
+            {"details___ref__demand__properties": {PROP_PAYMENT_TIMEOUT_SEC: 100}},
+            {"_base__payment_due_date": True},
+            lambda: datetime.now() - timedelta(seconds=300),
+            0,
+            3,
+            False,
+            True,
+        ),
+    ],
+)
+async def test_enforce_debit_note_intervals(
+    agreement_kwargs,
+    debit_note_kwargs,
+    created_at_cb,
+    num_debit_notes,
+    num_payable_debit_notes,
+    debit_note_accepted,
+    agreement_terminated,
+):
+    job = mock.AsyncMock()
+    agreement = AgreementFactory(**agreement_kwargs)
+    debit_note = DebitNoteFactory(_base__agreement_id=agreement.id, **debit_note_kwargs)
+    engine = mock_engine(agreement, debit_note, num_debit_notes, num_payable_debit_notes)
+    engine._activity_created_at[debit_note.activity_id] = created_at_cb()
+
+    assert await engine._enforce_debit_note_intervals(job, debit_note) == debit_note_accepted
+    assert job.agreements_pool._terminate_agreement.called == agreement_terminated
