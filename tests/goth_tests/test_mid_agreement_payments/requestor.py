fcd73ed1278506044152dd93f9ebb57dcbf95d6e
Filip <filip@golem.network>
2022-03-15 11:45:20 +0100

    Enforce shorter intervals and timeouts in market strategy

--- tests/goth_tests/test_mid_agreement_payments/requestor.py
+++ tests/goth_tests/test_mid_agreement_payments/requestor.py
@@ -7,12 +7,25 @@ from datetime import timedelta
 from yapapi import Golem, Task, WorkContext
 from yapapi.log import enable_default_logger, log_event_repr
 from yapapi.payload import vm
+from yapapi.props.builder import DemandBuilder
+from yapapi.strategy import LeastExpensiveLinearPayuMS, WrappingMarketStrategy
+
+
+class ShortDebitNoteIntervalAndPaymentTimeout(WrappingMarketStrategy):
+    def __init__(self, base_strategy):
+        super().__init__(base_strategy)
+
+    async def decorate_demand(self, demand: DemandBuilder) -> None:
+        await self.base_strategy.decorate_demand(demand)
+        demand.ensure(f"(PROP_DEBIT_NOTE_INTERVAL_SEC=15)")
+        demand.ensure(f"(PROP_PAYMENT_TIMEOUT_SEC=20)")
+        demand.ensure(f"(PROP_DEBIT_NOTE_ACCEPTANCE_TIMEOUT=10)")
 
 
 async def worker(context: WorkContext, tasks: AsyncIterable[Task]):
     async for task in tasks:
         script = context.new_script()
-        future_result = script.run("/bin/sh", "-c", "sleep 300")
+        future_result = script.run("/bin/sh", "-c", "sleep 100")
         yield script
         task.accept_result(result=await future_result)
 
@@ -23,7 +36,12 @@ async def main():
     tasks = [Task(data=None)]
     timeout = timedelta(hours=24)
 
-    async with Golem(budget=10.0, subnet_tag="goth", event_consumer=log_event_repr) as golem:
+    async with Golem(
+        budget=10.0,
+        strategy=ShortDebitNoteIntervalAndPaymentTimeout(LeastExpensiveLinearPayuMS()),
+        subnet_tag="goth",
+        event_consumer=log_event_repr,
+    ) as golem:
         logger = logging.getLogger("yapapi")
         logger.handlers[0].setLevel(logging.DEBUG)
         async for completed in golem.execute_tasks(worker, tasks, payload=package, timeout=timeout):
