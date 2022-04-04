85255b44e3428a274abe467802de628cc1d0c9af
Filip <filip@golem.network>
2022-04-04 16:37:57 +0200

    Remove CommandContainer test

--- tests/test_ctx.py
+++ tests/test_ctx.py
@@ -5,37 +5,8 @@ import pytest
 import sys
 from unittest import mock
 
-from yapapi.ctx import CommandContainer, WorkContext
+from yapapi.ctx import WorkContext
 from yapapi.script import Script
-
-
-def test_command_container():
-
-    c = CommandContainer()
-    c.deploy()
-    c.start(args=[])
-    c.transfer(_from="http://127.0.0.1:8000/LICENSE", to="container:/input/file_in")
-    c.run(entry_point="rust-wasi-tutorial", args=["/input/file_in", "/output/file_cp"])
-    c.transfer(_from="container:/output/file_cp", to="http://127.0.0.1:8000/upload/file_up")
-
-    expected_commands = """[
-        { "deploy": {} },
-        { "start": {"args": [] } },
-        { "transfer": {
-            "from": "http://127.0.0.1:8000/LICENSE",
-            "to": "container:/input/file_in"
-        } },
-        { "run": {
-            "entry_point": "rust-wasi-tutorial",
-            "args": [ "/input/file_in", "/output/file_cp" ]
-        } },
-        { "transfer": {
-            "from": "container:/output/file_cp",
-            "to": "http://127.0.0.1:8000/upload/file_up"
-        } }
-    ]
-    """
-    assert json.loads(expected_commands) == c.commands()
 
 
 @pytest.mark.skipif(sys.version_info < (3, 8), reason="AsyncMock requires python 3.8+")
