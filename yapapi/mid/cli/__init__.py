ec9499bf77e7db3e725e937990c5b72e7fa29d7d
Jan Betley <jan.betley@golem.network>
2022-07-12 15:23:47 +0200

    cli wip

--- yapapi/mid/cli/__init__.py
+++ yapapi/mid/cli/__init__.py
@@ -1,4 +1,23 @@
+import asyncio
+from functools import wraps
+
 import click
+
+from yapapi.payload import vm
+
+from yapapi.mid.golem_node import GolemNode
+
+
+def async_golem_wrapper(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        async def with_golem():
+            async with GolemNode() as golem:
+                await f(golem, *args, **kwargs)
+
+        loop = asyncio.get_event_loop()
+        return loop.run_until_complete(with_golem())
+    return wrapper
 
 
 @click.group()
@@ -11,33 +30,63 @@ def allocation():
     pass
 
 
-@cli.command()
-def status():
-    #   TODO
-    click.echo("STATUS")
+@allocation.command("list")
+@async_golem_wrapper
+async def allocation_list(golem):
+    allocations = await golem.allocations()
+    for allocation in allocations:
+        print(allocation)
 
 
-@cli.command()
-def find_node():
-    #   TODO
-    #   yapapi find-node --runtime vm --timeout 60s
-    click.echo("FIND NODES")
-
-
-@allocation.command()
-def list():
-    #   TODO
-    click.echo("ALLOCATION LIST")
-
-
-@allocation.command()
-def new():
+@allocation.command("new")
+@async_golem_wrapper
+async def allocation_new(golem):
     #   TODO
     #   yapapi allocation new 50 --network polygon
     click.echo("ALLOCATION NEW")
 
 
-@allocation.command()
-def clean():
+@allocation.command("clean")
+@async_golem_wrapper
+async def allocation_clean(golem):
+    for allocation in await golem.allocations():
+        await allocation.release()
+        print(allocation.id)
+
+
+@cli.command()
+@async_golem_wrapper
+async def status(golem):
     #   TODO
-    click.echo("ALLOCATION CLEAN")
+    click.echo("STATUS")
+
+
+@cli.command()
+@click.option(
+    "--runtime",
+    type=str,
+    required=True,
+)
+@click.option(
+    "--timeout",
+    type=int,
+    required=False,
+)
+@async_golem_wrapper
+async def find_node(golem, runtime, timeout):
+    click.echo(f"Looking for offers for runtime {runtime}")
+
+    async def get_nodes():
+        #   TODO: better payload creation (image hash?)
+        image_hash = "9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae"
+        payload = await vm.repo(image_hash=image_hash)
+
+        #   TODO: demand-as-contextmanager
+        demand = await golem.create_demand(payload)
+        async for offer in demand.offers():
+            print(offer)
+
+    try:
+        await asyncio.wait_for(get_nodes(), timeout)
+    except asyncio.TimeoutError:
+        pass
