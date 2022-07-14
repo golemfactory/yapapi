03b82eddc98880bdd592fddcac9773028d5b5cd6
Jan Betley <jan.betley@golem.network>
2022-07-14 16:36:48 +0200

    removed GolemNode.start()

--- example.py
+++ example.py
@@ -8,9 +8,6 @@ allocation_id = "a5f7be7b-2890-4a5f-a93e-d109849de7fb"
 demand_id = "32d198f1565f46aa917d704125b8e8ca-28e70eade4bffc91c3ab0b15b522878b47649169b27ae7659dffab4bb89ad0c0"
 image_hash = "9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae"
 
-golem = GolemNode()
-print(golem)
-
 
 async def example_1(golem):
     """Show existing allocation/demand"""
@@ -43,28 +40,30 @@ async def example_2(golem):
 
 
 async def example_3(golem):
-    """Create new allocation, demand, fetch a single offer, cleanup"""
-    async with golem:
-        allocation = await golem.create_allocation(1)
-        print(allocation)
+    """Create new allocation, demand, fetch a single offer, cleanup.
 
-        payload = await vm.repo(image_hash=image_hash)
-        demand = await golem.create_demand(payload, [allocation])
-        print(demand)
+    Also: this one works in non-contextmanager mode."""
+    allocation = await golem.create_allocation(1)
+    print(allocation)
 
-        async for offer in demand.offers():
-            print(offer)
-            break
+    payload = await vm.repo(image_hash=image_hash)
+    demand = await golem.create_demand(payload, [allocation])
+    print(demand)
 
-        await allocation.release()
-        await demand.unsubscribe()
+    async for offer in demand.offers():
+        print(offer)
+        break
+
+    await allocation.release()
+    await demand.unsubscribe()
+    await golem.aclose()
 
 
 async def example_4(golem):
     """EventBus usage"""
     from yapapi.mid import events
     from yapapi.mid.market import Offer
-    
+
     async def event_emitter(event) -> None:
         print("GOT EVENT", event)
 
@@ -75,6 +74,9 @@ async def example_4(golem):
 
 
 async def main():
+    golem = GolemNode()
+    print(golem)
+
     # print("\n---------- EXAMPLE 1 -------------\n")
     # await example_1(golem)
     print("\n---------- EXAMPLE 2 -------------\n")
