b6aa39bc87190ed521147efb41ce3147c4c18880
Jan Betley <jan.betley@golem.network>
2022-07-26 13:36:34 +0200

    events refreshed

--- yapapi/mid/events.py
+++ yapapi/mid/events.py
@@ -8,7 +8,7 @@ class Event(ABC):
     pass
 
 
-class ResourceEvent(ABC):
+class ResourceEvent(Event, ABC):
     def __init__(self, resource: Resource):
         self.resource = resource
 
@@ -16,27 +16,24 @@ class ResourceEvent(ABC):
         return f'{type(self).__name__}({self.resource})'
 
 
-class ResourceFound(ResourceEvent):
-    """Emitted on a first contact of the GolemNode with some already existing resource.
+class NewResource(ResourceEvent):
+    """Emitted when a new :class:`Resource` object is created.
 
-    This happens:
-    * When we find resource created by others, e.g. an Offer on the market.
-    * When we find resource created by our node, but not in this run, e.g.
-      await golem.allocation(allocation_id).get_data().
+    There are three distinct scenarios possible:
+    A) We create a new resource, e.g. with `GolemNode.create_allocation()`
+    B) We start interacting with some resource that was created by us before,
+       but not with this particular GolemNode instance, eg. GolemNode.allocation(allocation_id)
+    C) We find a resource created by someone else (e.g. a Proposal)
+
+    There's no difference between these scenarions from the POV of this event.
     """
 
 
-class ResourceCreated(ResourceEvent):
-    """Emitted when we create a new resource.
+class ResourceDataChanged(ResourceEvent):
+    """Emitted when `data` attribute of :class:`Resource` changes.
 
-    E.g. on
-    * await golem.create_allocation()
-    * [TODO] when the Agreement is first created
-    """
-
-
-class ResourceChanged(ResourceEvent):
-    """Emitted when we first notice a change in an already known resource.
+    Note that this event is **not** emitted when the data "would have changed if we
+    requested new data, but we didn't request it".
 
     Change might be caused by us or by some party.
     Second argument is the old data (before the change), so comparing
@@ -50,11 +47,26 @@ class ResourceChanged(ResourceEvent):
         self.old_data = old_data
 
 
+class ResourceChangePossible(ResourceEvent):
+    """Emitted when we receive an information that the resource.data might be outdated.
+
+    E.g. this is emitted for a `Proposal` when an agreement is created based on a proposal.
+
+    It is **not** guaranteed that anything really changed, but e.g. if you want to keep some :class:`Resource`
+    objects as up-to-date as possible, you might consider executing `await event.resorce.get_data(force=True)` on
+    every `ResourceChangePossible` event that references resources you are interested in.
+
+    NOTE: This is **NOT IMPLEMENTED** now, as it's pretty complex and useless now, and maybe not a good idea at all.
+    """
+
+
 class ResourceDeleted(ResourceEvent):
     """Emitted when we discover than a resource was deleted.
 
-    Usual case is when we delete a resource (e.g. await allocation.delete()),
-    but this can be also emitted when some other party deletes (TODO: what? offer?).
+    Usual case is when we delete a resource (e.g. `await allocation.release()`),
+    but this can be also emitted when we notice that resource was deleted by someone
+    else (TODO: when? is this possible at all? e.g. expired offer?)
 
     IOW, this is emitted whenever we start getting 404 instead of 200.
+    This event should never be emitted more than once for a given :class:`Resource`.
     """
