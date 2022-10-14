842d0c3b901430b790824d660a95d608a959e5c8
shadeofblue <blue@golem.network>
2022-10-14 15:15:47 +0200

    SocketProxy - a generalized TCP proxy for Services as a contrib element + generalized bytes/memoryview chunks generator

---
+++ tests/contrib/service/test_chunk.py
@@ -0,0 +1,33 @@
+import random
+from yapapi.contrib.service.chunk import chunks
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "data, chunk_limit, num_expected, mv",
+    (
+        (random.randbytes(16), 16, 1, True),
+        (random.randbytes(8), 16, 1, True),
+        (random.randbytes(17), 16, 2, True),
+        (random.randbytes(31), 16, 2, True),
+        (random.randbytes(256), 16, 16, True),
+        (random.randbytes(257), 16, 17, True),
+        (random.randbytes(16), 16, 1, False),
+        (random.randbytes(8), 16, 1, False),
+        (random.randbytes(17), 16, 2, False),
+        (random.randbytes(31), 16, 2, False),
+        (random.randbytes(256), 16, 16, False),
+        (random.randbytes(257), 16, 17, False),
+    ),
+)
+def test_chunks(data, chunk_limit, num_expected, mv):
+    num_received = 0
+    data_out = b""
+    data_in = memoryview(data) if data else data
+    for chunk in chunks(data_in, chunk_limit):
+        data_out += chunk
+        num_received += 1
+
+    assert num_received == num_expected
+    assert data_out == data
