c19f7e9251bf83b227a0c3fdc50151994944b2a1
Przemyslaw Walski <pwalski@users.noreply.github.com>
2023-04-17 14:27:31 +0200

    Last check of redirecting db output

--- examples/webapp/db/run_rqlite.sh
+++ examples/webapp/db/run_rqlite.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/bin/rqlited -http-addr 0.0.0.0:4001 /rqlite/file/data > /run/out 2> /run/err &
+/bin/rqlited -http-addr 0.0.0.0:4001 /rqlite/file/data > /root/.local/share/ya-provider/exe-unit/work/out.log 2> /root/.local/share/ya-provider/exe-unit/work/err.log &
