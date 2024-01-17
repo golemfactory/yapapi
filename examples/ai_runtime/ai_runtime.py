8e62076cb2bd7a9dc2b9ce107fb110387f11d17b
Przemyslaw Walski <pwalski@users.noreply.github.com>
2024-01-17 13:52:47 +0100

    Switch to v1-5-pruned-emaonly.safetensors Timeouts to let API start

--- examples/ai_runtime/ai_runtime.py
+++ examples/ai_runtime/ai_runtime.py
@@ -21,7 +21,7 @@ import asyncio
 import tempfile
 from datetime import datetime, timezone
 from pathlib import Path
-
+import time
 import colorama  # type: ignore
 
 from yapapi import Golem, NoPaymentAccountError
@@ -150,7 +150,7 @@ class ProviderOnceStrategy(MarketStrategy):
 
 # App
 
-RUNTIME_NAME = "ai"
+RUNTIME_NAME = "automatic"
 CAPABILITIES = "golem.runtime.capabilities"
 
 @dataclass
@@ -159,23 +159,20 @@ class AiPayload(Payload):
     image_fmt: str = prop("golem.!exp.ai.v1.srv.comp.ai.model-format", default="safetensors")
 
     runtime: str = constraint(inf.INF_RUNTIME_NAME, default=RUNTIME_NAME)
-    capabilities: str = constraint(CAPABILITIES, default="dummy")
+    capabilities: str = constraint(CAPABILITIES, default="automatic")
 
 
 class AiRuntimeService(Service):
     @staticmethod
     async def get_payload():
-        ## TODO switched into using smaller model to avoid problems during tests. Resolve it when automatic runtime integrated
         # return AiPayload(image_url="hash:sha3:92180a67d096be309c5e6a7146d89aac4ef900e2bf48a52ea569df7d:https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors?download=true")
-        return AiPayload(image_url="hash:sha3:0b682cf78786b04dc108ff0b254db1511ef820105129ad021d2e123a7b975e7c:https://huggingface.co/cointegrated/rubert-tiny2/resolve/main/model.safetensors?download=true")
+        return AiPayload(image_url="hash:sha3:6ce0161689b3853acaa03779ec93eafe75a02f4ced659bee03f50797806fa2fa:https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.safetensors?download=true")
     async def start(self):
         self.strategy.remember(self._ctx.provider_id)
 
         script = self._ctx.new_script(timeout=None)
         script.deploy()
         script.start(
-            "--model",
-            "dummy_model"
         )
         yield script
 
@@ -187,6 +184,9 @@ class AiRuntimeService(Service):
         self.strategy = strategy
 
 async def trigger(activity: RequestorControlApi, token, prompt, output_file):
+
+    #TODO Wait for the Automatic server to start.
+    time.sleep(30)
 
     custom_url = "/sdapi/v1/txt2img"
     url = activity._api.api_client.configuration.host + f"/activity/{activity.id}/proxy_http_request" + custom_url
@@ -241,7 +241,7 @@ async def main(subnet_tag, driver=None, network=None):
         print('Starting')
         print('Please input your prompt:')
         prompt = input()
-        await asyncio.sleep(3)
+        await asyncio.sleep(10)
         await get_image(
             prompt,
             'output.png'
